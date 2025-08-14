d1daae9950cb9aab6659133cbb7810101cc8ea45
Ubuntu <ubuntu@ip-172-31-27-159.ap-southeast-1.compute.internal>
2025-08-14 15:49:21 +0000

    update

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const port = 3000;
 
 // Create connection to MySQL
 const db = mysql.createConnection({
-    host: 'database-1.cp24ccc4chcf.ap-southeast-1.rds.amazonaws.com',
+    host: 'testdb-1.cp24ccc4chcf.ap-southeast-1.rds.amazonaws.com',
     user: 'root',
     password: '12345678',
     database: 'database_1'
