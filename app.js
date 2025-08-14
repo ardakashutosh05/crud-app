32d4e56e51557e1e890f0de0946e35e7d6c6ab55
Ubuntu <ubuntu@ip-172-31-27-159.ap-southeast-1.compute.internal>
2025-08-14 20:27:32 +0000

    data

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ app.delete('/deleteItem/:id', (req, res) => {
     });
 });
 
-app.listen(port, '0.0.0.0', () => {
+app.listen(port, "0.0.0.0", () => {
     console.log(`Server started on port ${port}`);
 });
 
