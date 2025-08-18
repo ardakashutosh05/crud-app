f6597646e6ac386011a36426f28068898a69a8b5
Ashutosh Sudhirrao Ardak <ardakashutosh05@gmail.com>
2025-08-18 13:02:10 +0530

    Update awscli2-kubectl-install.sh
    sctipt update

--- scripts/awscli2-kubectl-install.sh
+++ scripts/awscli2-kubectl-install.sh
@@ -45,3 +45,28 @@ mv ./kubectl ~/.local/bin/kubectl
 # Confirm kubectl installation
 echo "kubectl installation completed ✅"
 kubectl version --client
+
+# Download the latest version of eksctl
+echo "Downloading latest eksctl release..."
+# Replace amd64 with armv6, armv7 or arm64
+ (Get-FileHash -Algorithm SHA256 .\eksctl_Windows_amd64.zip).Hash -eq ((Get-Content .\eksctl_checksums.txt) -match 'eksctl_Windows_amd64.zip' -split ' ')[0]
+ ```
+
+#### Using Git Bash: 
+```sh
+# for ARM systems, set ARCH to: `arm64`, `armv6` or `armv7`
+ARCH=amd64
+PLATFORM=windows_$ARCH
+
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.zip"
+
+# (Optional) Verify checksum
+curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
+
+unzip eksctl_$PLATFORM.zip -d $HOME/bin
+
+rm eksctl_$PLATFORM.zip
+
+# Confirm eksctl installation
+echo "eksctl installation completed ✅"
+eksctl version -- client
